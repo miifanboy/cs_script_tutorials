4a5171753f4a81afc459fc59b0788a997c2f9e88
Mii Fanboy <87207112+miifanboy@users.noreply.github.com>
2025-10-05 18:52:22 +0300

    Update tutorial.js

--- 8 - Using instanceof/tutorial.js
+++ 8 - Using instanceof/tutorial.js
@@ -3,7 +3,7 @@ import { Instance, CSPlayerPawn } from "cs_script/point_script";
 
 Instance.OnGunFire((event) => {
 	var prt = event.weapon.GetParent();
-	if(parent instanceof CSPlayerPawn)
+	if(prt instanceof CSPlayerPawn)
 	{
 		var name = prt.GetPlayerController().GetPlayerName();
 		Instance.DebugScreenText(name,10,10,1,{r:255,g: 0, b: 0});
